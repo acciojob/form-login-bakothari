42fc4c63d8cd93565eeaacf2fbe4c8ca9c226e9c
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-20 23:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
-    //Write your code here
+let fm = document.querySelector("#form1")
+function getFormvalue(e) {
+    const formdata = {
+        fName: fm["fname"].value,
+        lName: fm["lname"].value,
 
+    }
+ let fullName = formdata["fName"] +" " + formdata["lName"]
+ alert(fullName)
 }
+fm.addEventListener("submit",getFormvalue)
+fm.removeEventListener("submit", getFormvalue); 
